08d270998be4007bb4bbb43d0fdfa949fd7941c5
Jozz77 <mmadujonathan@gmail.com>
2024-10-29 17:51:46 +0100

    new

--- arrays.js
+++ arrays.js
@@ -1,4 +1,18 @@
 
 const persons = ["Manasseh", "Edwin", 5, true];
-// console.log(persons)
-console.log(persons[1])
+
+// pop method which to remove the last value in the array
+persons.pop()
+
+// push method which to add a new value at the end of the array
+persons.push("Chidimma")
+
+// shift method which to remove the first value in the array
+persons.shift() 
+
+// unshift method which to add a new value at the beginning of the array
+persons.unshift("John")
+
+console.log(persons)
+// console.log(persons[1])
+// console.log(persons.length);
